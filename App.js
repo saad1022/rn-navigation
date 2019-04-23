0258d436445e98eed8b49b4e0d5442914cdfb758
Saad <sp18mcsw0006@maju.edu.pk>
2019-04-24 01:40:06 +0500

    All-Navigators-Combined

--- App.js
+++ App.js
@@ -1,83 +1,137 @@
 import  React  from 'react';
 import {Text , View , Button , ScrollView , StyleSheet} from 'react-native';
 
-import {createStackNavigator,createAppContainer,createDrawerNavigator} from 'react-navigation';
+import {createSwitchNavigator,createAppContainer,createDrawerNavigator,createBottomTabNavigator,createStackNavigator} from 'react-navigation';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-class Home extends React.Component {
-    //custom styling for screens in their classes
-    static navigationOptions = {
-        headerStyle: {
-            backgroundColor: 'blue'
-        }
-    }
+//Welcome Screen
+class WelcomeScreen extends React.Component {
     render(){
         return(
-            <View style={styles.container}>
-                <Text>Home</Text>
-                <Button
-                title="Go to Details"
-                onPress={()=> this.props.navigation.navigate('Details')}
-                />
-            </View>
+           <View style={styles.container}>
+               <Button title="Login" onPress={() => this.props.navigation.navigate('Dashboard')} />
+               <Button title="Sign Up" onPress={() => alert('button pressed')} />
+           </View>
+
+        );
+    }
+}
+//Dashboard Screen
+class DashboardScreen extends React.Component {
+    render(){
+        return(
+           <View style={styles.container}>
+               <Text>Dashboard Screen</Text>
+           </View>
+        );
+    }
+}
+//Feed Screen
+class FeedScreen extends React.Component {
+    render(){
+        return(
+           <View style={styles.container}>
+               <Text>Feed Screen</Text>
+           </View>
+        );
+    }
+}
+//Profile Screen
+class ProfileScreen extends React.Component {
+    render(){
+        return(
+           <View style={styles.container}>
+               <Text>Profile Screen</Text>
+           </View>
+        );
+    }
+}
+//Settings Screen
+class SettingsScreen extends React.Component {
+    render(){
+        return(
+           <View style={styles.container}>
+               <Text>Settings Screen</Text>
+           </View>
         );
     }
 }
 
-class DetailsScreen extends React.Component {
-    render(){
-        return(
-            <View style={styles.container}>
-            <Text>Details</Text>
-            <Button
-                title="Go to Details..again"
-                //push--go to req path again if already there
-                onPress={()=> this.props.navigation.push('Details')}
-            />
-            <Button
-                title="Go to Home"
-                //navigate--go to req path mentioned
-                onPress={()=> this.props.navigation.navigate('Home')}
-            />
-            <Button
-                title="Go Back"
-                onPress={()=> this.props.navigation.goBack()}
-            />
-            </View>
-        );
-    }
-}
-
-//Drawer Navigator
-const AppDrawNavigator = createDrawerNavigator(
-    {
-        //Path For Screens
-         Home: Home,
-         Details: DetailsScreen,
-    },
-    {
-        //default options for navigator
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: 'orange'
-            }
-        }
-    }
-);
-
-const AppContainer = createAppContainer(AppDrawNavigator);
-
-
-export default class App extends React.Component {
+class App extends React.Component {
     render(){
         return(
            <AppContainer />
         );
     }
 }
+export default App;
 
 
 
+
+//Bottom Tab Navigator
+const DashboardTabNavigator = createBottomTabNavigator(
+    {
+        //Path For Screens
+        Feed: FeedScreen,
+        Profile: ProfileScreen,
+        Settings: SettingsScreen
+    },
+    {
+        navigationOptions: ({ navigation }) => {
+            //Get the index of current active tab
+            const { routeName  } = navigation.state.routes[navigation.state.index];
+            return  {
+                headerTitle: routeName
+            };
+
+        }
+    }
+);
+
+//Dashboard Stack Navigator
+const DashboardStackNavigator = createStackNavigator(
+    {
+        //Path For Screens
+        DashboardTabNavigator: DashboardTabNavigator
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => {
+            return  {
+                headerLeft:(
+                    <Ionicons 
+                    style={{ paddingLeft: 10}} 
+                    onPress={() =>  navigation.openDrawer()} 
+                    name="md-menu" size={30} 
+                    />
+                ) 
+            };
+
+        }
+    }
+);
+
+//Drawer Navigator
+const AppDrawNavigator = createDrawerNavigator(
+    {
+        //Path For Screens
+        Dashboard: DashboardStackNavigator
+    }
+);
+
+//Switch Navigator
+const AppSwitchNavigator = createSwitchNavigator(
+    {
+        //Path For Screens
+    Welcome: WelcomeScreen,
+    Dashboard: AppDrawNavigator
+    }
+);
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
+
+//Styles
 const styles = StyleSheet.create({
     container: {
         flex: 1,
--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-Simple React Navigation Template
+Complx React Navigation Template
 
 
-Drawer Navigator basics used
+Switch + Drawer + Tab + Stack Navigator basics 
+
+
+- AppSwitchNavigator
+    - Welcome Screen
+        - Login Screen
+        - Sign Up Button
+
+      - AppDrawerNavigator
+            - Dashboard - DashboardStackNabigator(needed for and to change the header based on the tab)
+
+            - DashboardTabNavigator
+                - Tab 1 - FeedStack
+                - Tab 2 - ProfileStack
+                - Tab3 - SettingStack
+              - Any files you dont want to be a part of the tab  navigator can go here    
+
+
+
+ --- We have SWITCH NAVIGATOR which has DRAWER NAVIGATOR inside it 
+
+    - Inside DRAWER NAVIGATOR we have dashboard screen ---> DashBoard STACK NAVIGATOR
+
+        - Inside DashBoard STACK NAVIGATOR we have TAB NAVIGATOR
+
+                ++  TAB NAVIGATOR  have dynamic header
